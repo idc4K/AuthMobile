4652c32134adcbf5974e9209b00bc37a55f98432
idc4k <christikonga83@gmail.com>
2022-12-17 19:09:00 +0100

    start

--- AuthMobile/App.js
+++ AuthMobile/App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Routes from './components/Routes/Routes';
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Routes />
     </View>
   );
 }
